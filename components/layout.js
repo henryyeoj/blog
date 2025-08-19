ea301f9cfb95b039991fbcff8b8659e058e8cd67
henryyeoj <hysdelacruz.student@ua.edu.ph>
2025-08-19 12:17:31 +0800

    Updated

--- components/layout.js
+++ components/layout.js
@@ -1,69 +1,24 @@
 import Head from 'next/head';
-import Image from 'next/image';
-import styles from './layout.module.css';
+import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
-import Link from 'next/link';
  
-const name = 'Henry Yeoj Dela Cruz';
-export const siteTitle = 'Next.js Sample Website';
- 
-export default function Layout({ children, home }) {
+export default function Home() {
   return (
-    <div className={styles.container}>
+    <Layout home>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <meta
-          name=" Description "
-          content="Learn how to build a personal website using Next.js"
-        />
-        <meta
-          property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle,
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
-        <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
+        <title>{siteTitle}</title>
       </Head>
-      <header className={styles.header}>
-        {home ? (
-          <>
-            <Image
-              priority
-              src="/images/profile.jpg"
-              className={utilStyles.borderCircle}
-              height={144}
-              width={144}
-              alt=""
-            />
-            <h1 className={utilStyles.heading2Xl}>{name}</h1>
-          </>
-        ) : (
-          <>
-            <Link href="/">
-              <Image
-                priority
-                src="/images/profile.jpg"
-                className={utilStyles.borderCircle}
-                height={108}
-                width={108}
-                alt=""
-              />
-            </Link>
-            <h2 className={utilStyles.headingLg}>
-              <Link href="/" className={utilStyles.colorInherit}>
-                {name}
-              </Link>
-            </h2>
-          </>
-        )}
-      </header>
-      <main>{children}</main>
-      {!home && (
-        <div className={styles.backToHome}>
-          <Link href="/">← Back to home</Link>
-        </div>
-      )}
-    </div>
+      <section className={utilStyles.headingMd}>
+        <p>Hello! My name is Henry Yeo Dela Cruz. I’m excited to share a little about myself. I enjoy sports 
+          and going out for adventures with my friends, as these activities keep me active and allow me to make fun memories with the people around me. My favorite song is "Your Universe," which 
+          inspires me and keeps me motivated. When it comes to food, I love beef caldereta because of its rich and delicious flavors. In five years, I see myself becoming successful and happy, with a 
+          loving family by my side. I aim to continue growing both personally and professionally, and I look forward to building a bright and fulfilling future.
+</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
   );
 }
