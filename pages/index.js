04c292e3d9e4882baae18cd3c58556fe2b62364f
henryyeoj <hysdelacruz.student@ua.edu.ph>
2025-08-22 14:19:16 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello! My name is Henry Yeo Dela Cruz. I’m excited to share a little about myself. I enjoy sports 
+        <p>Hello! My name is Henry Yeoj Dela Cruz. I’m excited to share a little about myself. I enjoy sports 
           and going out for adventures with my friends, as these activities keep me active and allow me to make fun memories with the people around me. My favorite song is "Your Universe," which 
           inspires me and keeps me motivated. When it comes to food, I love beef caldereta because of its rich and delicious flavors. In five years, I see myself becoming successful and happy, with a 
           loving family by my side. I aim to continue growing both personally and professionally, and I look forward to building a bright and fulfilling future.
